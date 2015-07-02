697f20df810140776049a37e9e53007d46f60183
KBryan <kwame@3einteractive.com>
2015-07-02 12:10:13 -0400

    Comparison Operator and String Interpolation

--- iOSDay1.playground/Contents.swift
+++ iOSDay1.playground/Contents.swift
@@ -35,6 +35,8 @@ Types can also be Objects. More on that later. In our last example, we let the c
 To be explicit we will create our variables a bit differently as an example we would create our variables like the following.               
     **var myFloatingPointValue:Float = 234.56**
 
+[Helpful resourses on Data Types should be read before continuing.](https://developer.apple.com/library/mac/documentation/Swift/Conceptual/Swift_Programming_Language/TheBasics.html)
+
 ## Challenge two: Create 4 Variables that store a Boolean Value / an Integer Value / a Floating Point Value / and a String Value
 
 */
@@ -88,5 +90,66 @@ If you want to add or subtract 1 from a number, there are special operators just
 * var b = 10
 * ++b
 * --b
+
+*/
+
+
+/*:
+## Comparison operators
+
+Swift has a set of operators that perform comparisons on values. 
+ 
+greater than **(>)** , greater than or equal **(>=)** , and less than **(<)**  less than or equal **(<=)**.
+
+If you want to check for equality, you can't use = because already has a meaning: it's used to give a variable a value. So, Swift has an alternative in the form of ==, meaning "is equal to." For example:
+Type the following into playground and see the result.
+
+* var a = 1.1
+* var b = 2.2
+* var c = a + b
+
+* c > 3
+* c >= 3
+* c > 4
+* c < 4
+
+*/
+
+
+/*:
+## String interpolation
+
+This is a fancy name for what is actually a very simple thing: combining variables and constants inside a string.
+
+* var name = "Your Name"
+If we wanted to print out a message to the user that included their name, string interpolation is what makes that easy: you just write a backslash, then an open parenthesis, then your code, then a close parenthesis, like this:
+
+* var name = "Your Name"
+* "Your name is \(name)"
+
+The results pane will now show "Your name is Your Name" all as one string, because string interpolation combined the two for us.
+
+Now, we could have written that using the + operator, like this:
+
+var name = "Your Name"
+"Your name is " + name
+
+…but that's not as efficient, particularly if you're combining multiple variables together. In addition, string interpolation in Swift is smart enough to be able to handle a variety of different data types automatically. For example:
+
+* var name = "Your Name"
+* var age = 25
+* var latitude = 36.166667
+
+"Your name is \(name), your age is \(age), and your latitude is \(latitude)"
+
+## Challenge 3
+* Create a variable of type string for your name 
+* Create a variable of type int for your age
+* using string interpolation print both of these in the same line.
+
+*/
+
+
+/*:
 
 */
