127f3b3a3d5eb60aaa10a8a7d0622cc375f2331d
KBryan <kwame@3einteractive.com>
2015-07-02 11:07:59 -0400

    Initial Playground for Day 1
    Will continually update throughout the day

---
+++ iOSDay1.playground/Contents.swift
@@ -0,0 +1,51 @@
+import UIKit
+
+/*:
+
+# Bitmaker Labs Assignment 1
+
+Welcome to BitMakerLabs today you'll be introduced to the Swift Programming Language.
+ The first thing we'll look at is a variable.
+ Variables can be defined as a container that stores information
+ In the example below we have a variable that stores a string
+ Creating a variable starts by using the keyword **var** next you define the name of the variable **str** the **=** is an assignment operator this tells the compiler that the value to the right of the **=** will now be stored in variable **str**
+*/
+var str = "Hello, playground"
+/*:
+
+Challenge 1: Create a variable that stores your first and last name
+Note: **var** defines that your creating a variable
+**myName** defines the name of the varaible
+**=** defines an assignment operator
+**"MyName"** is the string value that you will store
+
+*/
+
+
+/*:
+Congratulations on creating your first variable. There's many different variable that you can create. An important concept that we must discuss is types. **Types** define the values that can be stored in a variable. A list of different value types that you can work with are as follows.
+
+**Bool** defines true or false values
+**Int** defines whole numbers example 12 or 234
+**Float** defines defines floating point values 12.5 or 234.567
+**String** defines a series of characters "Kwame Bryan"
+
+Types can also be Objects. More on that later. In our last example we let the compiler make that decision about the types we were working with. We can also be explicit about our types.
+
+To be explicit we will create our variables a bit differently as an example we would create our variables like the following.               
+    **var myFloatingPointValue:Float = 234.56**
+
+## Challenge two: Create 4 Variables that store a Boolean Value / an Integer Value / a Floating Point Value / and a String Value
+
+*/
+
+
+
+
+
+/*:
+We've talked about variables and their uses. In some cases you'll not want a variable to be changed in these cases you will want to use the keyword **let** which will create a constant or sometimes referred to as a immutable type. An example of an immutable type is as follows
+
+**let variableThatShouldChange:String = "SomeString"**. Now, you have a try at this. Create a variable that is constant. Also, try and assign a value to that variable after you have declared it. What type of error do you recieve.
+
+*/
